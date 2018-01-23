4e333799d203c9a2184b67209d662f90ff5f6ada
Ronald G. Minnich <rminnich@gmail.com>
2018-01-22 18:25:54 -0800

    ops2.go: optab
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- ops2.go
+++ ops2.go
@@ -1673,7 +1673,7 @@ func x86emuOp2_movsx_word_R_RM(_ u8) {
 /***************************************************************************
  * Double byte operation code table:
  **************************************************************************/
-void x86emu_optab2[256] =
+void x86emu_optab2 = [256]optab {
 {
 /*  0x00 */ x86emuOp2_illegal_op,  /* Group F (ring 0 PM)      */
 /*  0x01 */ x86emuOp2_opc_01,      /* Group G (ring 0 PM)      */
