5042610740f7b9a5e9cee886e6d6404c947352b8
Ronald G. Minnich <rminnich@gmail.com>
2018-01-25 10:05:33 -0800

    add fpu.go
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ fpu.go
@@ -0,0 +1,33 @@
+package main
+
+func x86emuOp_esc_coprocess_d8(_ uint8) {
+	panic("x86emuOp_esc_coprocess_d8")
+}
+
+func x86emuOp_esc_coprocess_d9(_ uint8) {
+	panic("x86emuOp_esc_coprocess_d9")
+}
+
+func x86emuOp_esc_coprocess_da(_ uint8) {
+	panic("x86emuOp_esc_coprocess_da")
+}
+
+func x86emuOp_esc_coprocess_db(_ uint8) {
+	panic("x86emuOp_esc_coprocess_db")
+}
+
+func x86emuOp_esc_coprocess_dc(_ uint8) {
+	panic("x86emuOp_esc_coprocess_dc")
+}
+
+func x86emuOp_esc_coprocess_dd(_ uint8) {
+	panic("x86emuOp_esc_coprocess_dd")
+}
+
+func x86emuOp_esc_coprocess_de(_ uint8) {
+	panic("x86emuOp_esc_coprocess_de")
+}
+
+func x86emuOp_esc_coprocess_df(_ uint8) {
+	panic("x86emuOp_esc_coprocess_df")
+}
