8ccb173de8a954b3f039a179bafa8f25222b84c7
Ronald G. Minnich <rminnich@gmail.com>
2018-02-22 21:58:47 -0800

    first shll tests; 1360 now pass
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- tcg/tests-i386-shift.h
+++ tcg/tests-i386-shift.h
@@ -43,17 +43,16 @@
 	.asciz "%s%s A=%08x B=%08x R=%08x CCIN=%04x CC=%04x" ;
 
 #else
-#define EXECSHIFT(size, rsize, res, s1, s2, flags) \
+#define EXECSHIFT(o, size, rsize, bits, pre, res, s1, s2, flags) \
 	movw	$flags, %dx ;\
 	pushw %dx ;\
 	popf; \
 	OPR(mov,l) $res, REG(a, e, x);	\
 	PUSH(a,e) ;\
-	OPR(mov,l) $s1, REG(b, e, x) ;	\
-	PUSH(b,e) ;\
-	         stringify(OP) size " %%cl, %" rsize "0\n\t" \
+	OPR(mov,l) $s1, REG(c, e, x) ;	\
+	PUSH(c,e) ;\
 	OPR(o,size) %cl, REG(a,pre, rsize) ;	\
-	PUSH(c,e) ;					\
+	PUSH(a,e) ;					\
 	pushf ;\
 	movw	$flags, %dx ;\
 	cmpb $1, %al;\
@@ -74,78 +73,32 @@
 
 #else
 #define EXECSHIFT(size, rsize, res, s1, s2, flags) \
-    asm ("push %4\n\t"\
-         "popf\n\t"\
-         stringify(OP) size " %%cl, %" rsize "5, %" rsize "0\n\t" \
-         "pushf\n\t"\
-         "pop %1\n\t"\
-         : "=g" (res), "=g" (flags)\
-         : "c" (s1), "0" (res), "1" (flags), "r" (s2));
-
-void exec_opl(long s2, long s0, long s1, long iflags)
-{
-    long res, flags;
-    res = s0;
-    flags = iflags;
-    EXECSHIFT("l", "k", res, s1, s2, flags);
-    /* overflow is undefined if count != 1 */
-    if (s1 != 1)
-      flags &= ~CC_O;
-    result9(8, "%-10s A=" FMTLX " B=" FMTLX " C=" FMTLX " R=" FMTLX " CCIN=%04lx CC=%04lx\n",
-           stringify(OP) "l", s0, s2, s1, res, iflags, flags & CC_MASK);
-}
-
-void exec_opw(long s2, long s0, long s1, long iflags)
-{
-    long res, flags;
-    res = s0;
-    flags = iflags;
-    EXECSHIFT("w", "w", res, s1, s2, flags);
-    /* overflow is undefined if count != 1 */
-    if (s1 != 1)
-      flags &= ~CC_O;
-    result9(8, "%-10s A=" FMTLX " B=" FMTLX " C=" FMTLX " R=" FMTLX " CCIN=%04lx CC=%04lx\n",
-           stringify(OP) "w", s0, s2, s1, res, iflags, flags & CC_MASK);
-}
-
-#endif
-
-#ifndef OP_NOBYTE
-void exec_opb(long s0, long s1, long iflags)
-{
-    long res, flags;
-    res = s0;
-    flags = iflags;
-    EXECSHIFT("b", "b", res, s1, 0, flags);
-    /* overflow is undefined if count != 1 */
-    if (s1 != 1)
-      flags &= ~CC_O;
-    result(7, "%-10s A=" FMTLX " B=" FMTLX " R=" FMTLX " CCIN=%04lx CC=%04lx\n",
-           stringify(OP) "b", s0, s1, res, iflags, flags & CC_MASK);
-}
+	movw	$flags, %dx ;\
+	pushw %dx ;\
+	popf; \
+	OPR(mov,l) $res, REG(a, e, x);	\
+	PUSH(a,e) ;\
+	OPR(mov,l) $s1, REG(c, e, x) ;	\
+	PUSH(c,e) ;\
+	OPR(o,size) %cl, REG(a,pre, rsize) ;	\
+	 PUSH(b,e) ; \
+	PUSH(a,e) ;\
+	pushf ;\
+	movw	$flags, %dx ;\
+	cmpb $1, %al;\
+	je 1f; \
+	andw $0x800, %dx;		\
+1: \
+	pushw %dx ;\
+	hlt ;\
+	.byte 3; /* number of following bytes of info */ \
+	/* currently # bits per stack item, and nargs */ \
+	.byte bits, 4; \
+	.asciz #o ;							\
+	.asciz "%s%s A=%08x B=%08x C=%08lx R=%08x CCIN=%04x CC=%04x" ;
 #endif
 
 #if 0
-void exec_op(long s2, long s0, long s1)
-{
-    s2 = i2l(s2);
-    s0 = i2l(s0);
-    exec_opl(s2, s0, s1, 0);
-#ifdef OP_SHIFTD
-    exec_opw(s2, s0, s1, 0);
-#else
-    exec_opw(s2, s0, s1, 0);
-#endif
-#ifndef OP_NOBYTE
-    exec_opb(s0, s1, 0);
-#endif
-#ifdef OP_CC
-    exec_opl(s2, s0, s1, CC_C);
-    exec_opw(s2, s0, s1, CC_C);
-    exec_opb(s0, s1, CC_C);
-#endif
-}
-
 void glue(test_, OP)(void)
 {
     int i, n;
